b5fc24618324a3024ec8ec5be0aae6c97b7ce33d
elrilacho <camilobr17@gmail.com>
2023-03-29 09:47:45 -0500

    Componente listo

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -22,7 +22,7 @@
 
     figure
       .video
-        iframe(width='560' height='315' src='https://www.youtube.com/embed/2L91WMqw96A' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen='allowfullscreen')
+        iframe(width='560' height='315' src='https://www.youtube.com/embed/4G2Jr5Uo428' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen='allowfullscreen')
 
     
 </template>
